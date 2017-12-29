80af85d856149d79ed64cfccccf6d3f1487af088
Darshan999 <darshandhandhukiya61@gmail.com>
2017-12-29 13:35:32 +0530

    pacing

--- application/models/packingtype.php
+++ application/models/packingtype.php
@@ -3,7 +3,7 @@ class Packingtype extends CI_Model
 {
 	public function fetchpack()
 	{
-		$q=$this->db->query('select * from paking_type_tbl');
+		$q=$this->db->query('select * from packing_type_tbl');
 		return $q->result();
 	}
 }
