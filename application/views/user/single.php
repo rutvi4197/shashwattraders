0109f027e5989d5ad46adedfdc4eb8d7c41e97ac
Cool_Dude_Priyansh <priyanshsheth1997@gmail.com>
2017-12-29 15:00:57 +0530

    product page

--- application/views/user/single.php
+++ application/views/user/single.php
@@ -11,13 +11,15 @@
 		<div class="single">
 			<div class="container">
 						<div class="single-top-main">
+							 <?php if(count($product_detail) ):
+               foreach($product_detail as $product_detail):?>
 	   		<div class="col-md-5 single-top">
 	   		<div class="single-w3agile">
 							
 <div id="picture-frame">
-			<img src="<?php echo base_url();?>/application/assets/images/si.jpg" data-src="<?php echo base_url();?>/assets/images/si-1.jpg" alt="" class="img-responsive"/>
+			<img src="<?php echo  base_url().'/application/assets/'.$product_detail->product_photo; ?>" data-src="<?php echo  base_url().'/application/assets/'.$product_detail->product_photo; ?>" alt="" class="img-responsive"/>
 		</div>
-										<script src="<?php echo base_url();?>/assets/js/jquery.zoomtoo.js"></script>
+										<script src="<?php echo base_url();?>/application/assets/js/jquery.zoomtoo.js"></script>
 								<script>
 			$(function() {
 				$("#picture-frame").zoomToo({
@@ -63,7 +65,10 @@
 		   <div class="clearfix"> </div>
 	   </div>	
 				 
-				
+				   <?php endforeach;
+                  endif;
+                    
+               ?>
 	</div>
 </div>
 		<!---->
